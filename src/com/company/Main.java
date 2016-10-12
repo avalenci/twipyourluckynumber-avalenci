a764872bd65a8aa08d2530cd56f6d516039e4fd8
avalenci <ansmeew@gmail.com>
2016-10-12 00:57:59 -0400

    Third commit.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,26 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        while (true) {
+        boolean b = true;
+        while (b) {
             System.out.println("Could I acquire your name?");
             Perfect person = new Perfect(input.nextLine());
+            int luckyNumber = person.perfectRandom();
             System.out.println("Nice to meet you " + person.name +
-                    "! Your lucky number is " + person.perfectRandom() + ".");
-            System.out.println();
+                    "! Your lucky number is " + luckyNumber + ".");
+            while (true) {
+                System.out.println("\nType (0) to exit.\nType (1) for your name and number." +
+                        "\nType (2) to enter another name.");
+                String a = input.nextLine().trim();
+                if (a.equals("0")) {
+                    b = false;
+                    break;
+                }
+                else if (a.equals("1"))
+                    System.out.println("Your name is " + person.name + " and your lucky number is " + luckyNumber);
+                else if (a.equals("2"))
+                    break;
+            }
         }
     }
 }
